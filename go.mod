d5bd8fc7fc6fb37d5c4e3be93f911d0bbb925c0e
Faridi17 <azkafaridi@gmail.com>
2024-09-11 21:21:57 +0700

    create my module

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Faridi17/go-mymodules
+
+go 1.23.0
---
+++ say_hello.go
@@ -0,0 +1,13 @@
+package go_say_hello
+
+func SayHello() string {
+	return "Hello World"
+}
+
+func SayName(name string) string{
+	return "Hello " + name
+}
+
+func MathPlus(a, b int) int {
+	return a + b
+}
