04c8c158867c071bf88aa137d425101b6882af17
Faridi17 <azkafaridi@gmail.com>
2024-09-11 21:49:09 +0700

    update module say hello

--- say_hello.go
+++ say_hello.go
@@ -8,6 +8,28 @@ func SayName(name string) string{
 	return "Hello " + name
 }
 
+func getCompleteName() (firstName, middleName, lastName string) {
+	firstName = "Muhammad"
+	middleName = "Azka"
+	lastName = "Faridi"
+
+	return firstName, middleName, lastName
+}
+
 func MathPlus(a, b int) int {
 	return a + b
+}
+
+func getFullName() (string, string) {
+	return "Azka", "Faridi"
+}
+
+func sumAll(numbers ...int) int {
+	total := 0
+
+	for _, number := range numbers {
+		total += number
+	}
+
+	return total
 }
